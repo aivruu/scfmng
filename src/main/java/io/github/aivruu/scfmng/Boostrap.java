48606bbdf8f37d0243395b51f4f52b40944267f0
aivruu <benjaalejandro206@gmail.com>
2025-07-01 20:53:55 -0400

    chore(Boostrap): rename function to 'main()' to allow program start-up

--- src/main/java/io/github/aivruu/scfmng/Boostrap.java
+++ src/main/java/io/github/aivruu/scfmng/Boostrap.java
@@ -1,7 +1,13 @@
 package io.github.aivruu.scfmng;
 
+/**
+ * First entry-point for the application, any required check is performed here before call to the
+ * {@link Main#main(String[])} function.
+ *
+ * @since 0.0.1
+ */
 public final class Boostrap {
-  public static void start(final String[] args) {
+  public static void main(final String[] args) {
     // first entry-point, check if required database-driver is included for database-connection,
     // otherwise, stop application start-up.
     try {
